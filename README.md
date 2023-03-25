46e86b5d01fd5d60733e3ef4a69d7b0b48c426a2
Bloodov Pavel <kasper6024@gmail.com>
2023-03-25 21:12:16 +0300

    Добавлен модуль environs для секретных переменных

--- README.md
+++ README.md
@@ -1,13 +1,29 @@
 # Пульт охраны банка
-
+Показывает сотрудников с активными карточками-пропусками, кто в хранилище на данный момент и все визиты по карточке-пропуску
 ### Как пользоваться
 Подразумевается, что Python у вас установлен :
 1. Скачать скрипт
 2. Установить requirements.txt, введя в консоли 
-``` 
+```buildoutcfg
 pip install requirements.txt
 ```
-3. Запустить файл `main.py`
+3. Создать файл `.env` 
+4. В файле `.env` записать данные для подключения к БД:
+
+ ```buildoutcfg
+DB_ENGINE=YOUR_DB_ENGINE
+DB_HOST=YOUR_DB_HOST
+DB_PORT=YOUR_DB_PORT_int
+DB_NAME=YOUR_DB_NAME
+DB_USER=YOUR_DB_USER
+DB_PASSWORD=YOUR_DB_PASSWORD
+SECRET_KEY=YOUR_SECRET_KEY
+DEBUG=BOOL (true or false)
+```
+5. Запустить файл введя в консоли
+```buildoutcfg
+python manage.py runserver
+```
 
 Для изменения времени подозрительных визитов в файле `passcard_info_view.py` необходимо указать нужное кол-во в минутах 
 ```py
--- project/settings.py
+++ project/settings.py
@@ -1,21 +1,34 @@
 import os
+from environs import Env
+
+env = Env()
+# Read .env into os.environ
+env.read_env()
+
+db_engine = env('DB_ENGINE')
+db_host = env('DB_HOST')
+db_port = env.int('DB_PORT')
+db_name = env('DB_NAME')
+db_user = env('DB_USER')
+db_password = env('DB_PASSWORD')
+debug = env.bool('DEBUG', default=False)
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'HOST': 'checkpoint.devman.org',
-        'PORT': '5434',
-        'NAME': 'checkpoint',
-        'USER': 'guard',
-        'PASSWORD': 'osim5',
+        'ENGINE': db_engine,
+        'HOST': db_host,
+        'PORT': db_port,
+        'NAME': db_name,
+        'USER': db_user,
+        'PASSWORD': db_password,
     }
 }
 
 INSTALLED_APPS = ['datacenter']
 
-SECRET_KEY = 'REPLACE_ME'
+SECRET_KEY = os.environ['SECRET_KEY']
 
-DEBUG = True
+DEBUG = debug
 
 ROOT_URLCONF = 'project.urls'
 
